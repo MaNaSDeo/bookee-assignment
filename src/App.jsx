571571c9d79172e5884d6acdfdf10c1ad02604da
MaNaSDeo <102231033+MaNaSDeo@users.noreply.github.com>
2024-01-10 12:46:20 +0530

    Backend API setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-function App() {
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-  return (
-    <>
-      Hello
-    </>
-  )
+function App() {
+  const [data, setData] = useState([]);
+  console.log(data);
+  const api_URL = "http://127.0.0.1:8080/shifts";
+
+  const handleApi = async (api_URL) => {
+    try {
+      const response = await axios.get(api_URL);
+      if (response.status === 200) {
+        setData(response.data);
+      }
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
+
+  useEffect(() => {
+    handleApi(api_URL);
+  }, []);
+
+  return <>Hello</>;
 }
 
-export default App
+export default App;
